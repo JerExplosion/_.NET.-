66b015b9cda80913494e706cd48ec58364891f5a
Jerry Ren <jerryren.ios@gmail.com>
2020-10-05 23:59:20 -0400

    G-interfaceExtraction

--- GemoHub/GemoHub/EmotiClass.cs
+++ GemoHub/GemoHub/EmotiClass.cs
@@ -1,23 +1,23 @@
-﻿using System;
-namespace GemoHub
-{
-    public class EmotiClass
-    {
+﻿//using System;
+//namespace GemoHub
+//{
+//    public class EmotiClass
+//    {
 
-        public string Key
-        {
-            get;
-            set;
-        }
+//        public string Key
+//        {
+//            get;
+//            set;
+//        }
 
-        public string URL
-        {
-            get;
-            set;
-        }
+//        public string URL
+//        {
+//            get;
+//            set;
+//        }
 
-        public EmotiClass()
-        {
-        }
-    }
-}
+//        public EmotiClass()
+//        {
+//        }
+//    }
+//}
--- GemoHub/GemoHub/GithubEmojiService.cs
+++ GemoHub/GemoHub/GithubEmojiService.cs
@@ -6,23 +6,29 @@ using System.Net.Http;
 
 namespace GithubEmojis {
 
-    public class GithubEmojiService : IGithubEmojiService {
+    public class GithubEmojiService : IGithubEmojiService
+    {
         const string GithubEmojiUrl = "https://api.github.com/emojis";
         private readonly HttpClient _httpClient;
-        private IList<Emoji> _emojis;
+        private IList<Emoji> _emojis; //改成 emoemp?
 
-        public GithubEmojiService() {
+        public GithubEmojiService()
+        {
             _httpClient = new HttpClient();
             _httpClient.DefaultRequestHeaders.Add("User-Agent", "sayedihashimi-github-emojis");
         }
 
-        public async Task<IList<Emoji>> GetEmojis() {
-            if(_emojis == null || _emojis.Count <= 0) {
+        public async Task<IList<Emoji>> GetEmojis()
+        {
+            if (_emojis == null || _emojis.Count <= 0)
+            {
                 var emojiStr = await _httpClient.GetStringAsync(GithubEmojiUrl);
-                try {
+                try
+                {
                     _emojis = GetEmojisFrom(emojiStr);
                 }
-                catch(Exception ex) {
+                catch (Exception ex)
+                {
                     System.Console.WriteLine($"error: {ex.ToString()}");
                 }
             }
@@ -30,14 +36,17 @@ namespace GithubEmojis {
             return _emojis;
         }
 
-        public IList<Emoji> GetEmojisFrom(string content) {
+        public IList<Emoji> GetEmojisFrom(string content)
+        {
             var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
             var results = new List<Emoji>();
 
-            foreach (var key in dictionary.Keys) {
+            foreach (var key in dictionary.Keys)
+            {
                 if (string.IsNullOrWhiteSpace(key)) { continue; }
 
-                results.Add(new Emoji {
+                results.Add(new Emoji
+                {
                     Key = key,
                     Url = dictionary[key]
                 });
