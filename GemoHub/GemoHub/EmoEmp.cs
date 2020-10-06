c2880e6ad6402a3d6562e882794572708ac53bcb
Jerry Ren <jerryren.ios@gmail.com>
2020-10-05 23:49:41 -0400

    mojito-service

---
+++ GemoHub/GemoHub/EmoEmp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GemoHub
+{
+    public class EmoEmp
+    {
+        public string Key {
+            get;
+            set;
+        }
+        public string Url {
+            get;
+            set;
+        }
+    }
+}
